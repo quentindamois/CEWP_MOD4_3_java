24e00c7cb623732d53e006700f19044eb1e97de9
quentin damois <quentin.damois@efrei.net>
2023-09-17 14:23:41 -0400

    Completion of what is considerred the basic requirement for the third exercise. Comment and cleaning need to be done.

---
+++ src/main/java/com/example/bouncingballjavafx/AbleToBounce.java
@@ -0,0 +1,90 @@
+package com.example.bouncingballjavafx;
+
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+public class AbleToBounce extends Circle {
+    private int xSpeed;
+    private int ySpeed;
+    public AbleToBounce() {
+        super();
+    }
+    public AbleToBounce(double radius) {
+        super(radius);
+    }
+    public AbleToBounce(double centerX, double centerY, double radius, int xSpeed, int ySpeed) {
+        super(centerX, centerY, radius);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public AbleToBounce(double radius, Paint fill) {
+        super(radius, fill);
+    }
+    public AbleToBounce(double centerX, double centerY, double radius, Paint fill) {
+        super(centerX, centerY, radius, fill);
+    }
+    /*
+    public AbleToBounce(double centerX, double centerY) {
+        super(centerX, centerY);
+    }
+     */
+    public void setXSpeed(int xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+    public void setYSpeed(int ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    public int getXSpeed() {
+        return this.xSpeed;
+    }
+    public int getYSpeed() {
+        return this.ySpeed;
+    }
+    public double[] getNort() {
+        double[] res = { this.getCenterX(), this.getCenterY() - this.getRadius() * 2};
+        return res;
+    }
+    public double[] getNorthEast() {
+        double[] res = {this.getCenterX() + this.getRadius() * 2, this.getCenterY() - this.getRadius() * 2};
+        return res;
+    }
+    public double[] getEast() {
+        double[] res = {this.getCenterX() + this.getRadius() * 2, this.getCenterY()};
+        return res;
+    }
+    public double[] getSouthEast() {
+        double[] res = {this.getCenterX() + this.getRadius() * 2, this.getCenterY() + this.getRadius() * 2};
+        return res;
+    }
+    public double[] getSouth() {
+        double[] res = {this.getCenterX(), this.getCenterY() + this.getRadius() * 2};
+        return res;
+    }
+    public double[] getSouthWest() {
+        double[] res = {this.getCenterX() - this.getRadius() * 2, this.getCenterY() + this.getRadius() * 2};
+        return res;
+    }
+    public double[] getWest() {
+        double[] res = {this.getCenterX() - this.getRadius() * 2, this.getCenterY()};
+        return res;
+    }
+    public double[] getNortWest() {
+        double[] res = {this.getCenterX() - this.getRadius() * 2, this.getCenterY() - this.getRadius() * 2};
+        return res;
+    }
+    public boolean Colide(AbleToBounce ball) {
+        return this.isIn(ball.getNort()) || this.isIn(ball.getNortWest()) || this.isIn(ball.getWest()) || this.isIn(ball.getSouthWest()) || this.isIn(ball.getSouth()) || this.isIn(ball.getSouthEast()) || this.isIn(ball.getEast()) || this.isIn(ball.getNorthEast());
+    }
+    public boolean isIn(double[] point) {
+        return ((point[0] - this.getCenterX()) * (point[0] - this.getCenterX()) + (point[1] - this.getCenterY()) * (point[1] - this.getCenterY()) <= ((this.getRadius()/2) * (this.getRadius()/2)));
+    }
+    public boolean isBorderColide(int windowWidth, int windowHeight, double[] point) {
+        return point[0] <= -50 || point[0] >= windowWidth || point[1] <= -50 || point[1] >= windowHeight;
+    }
+    public boolean iSBorderColideAll(int windowWidth, int windowHeigth) {
+        return this.isBorderColide(windowWidth, windowHeigth, this.getNort()) || this.isBorderColide(windowWidth, windowHeigth, this.getNortWest()) || this.isBorderColide(windowWidth, windowHeigth, this.getWest()) || this.isBorderColide(windowWidth, windowHeigth, this.getSouthWest()) || this.isBorderColide(windowWidth, windowHeigth, this.getSouth()) || this.isBorderColide(windowWidth, windowHeigth, this.getSouthEast()) || this.isBorderColide(windowWidth, windowHeigth, this.getEast()) || this.isBorderColide(windowWidth, windowHeigth, this.getNorthEast());
+    }
+    public String toString() {
+        return "Center : (" + this.getCenterX() + ", " + this.getCenterY() + ")\nRadius : " + this.getRadius() + "\nSpeed on the x axis : " + this.xSpeed + "\nSpeed on the y axis : " + this.ySpeed;
+    }
+}
--- src/main/java/com/example/bouncingballjavafx/Ball.java
+++ src/main/java/com/example/bouncingballjavafx/Ball.java
@@ -1,49 +1,112 @@
 package com.example.bouncingballjavafx;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Ball extends Application {
-    private Circle ballThatBounce;
+    private ArrayList<AbleToBounce> ballThatBounce;
+    private GridPane ballgroup;
     private final int WINDOW_WIDTH = 500;
     private final int WINDOW_HEIGHT = 500;
+    private ExecutorService executor;
     @Override
     public void start(Stage stage) throws IOException {
         Label title = new Label("click anywhere to throw the ball.");
-        Scene scene = new Scene(title, WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.ballThatBounce = new ArrayList<AbleToBounce>();
+       this.ballgroup = new GridPane();
+       this.ballgroup.setVgap(1.0);
+       this.ballgroup.setHgap(1.0);
+       this.ballgroup.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+           System.out.println("Working");
+           bouncing(event.getSceneX(), event.getSceneY());
+       });
+       this.executor = Executors.newFixedThreadPool(20);
+       this.executor.submit(() -> {
+           actualise();
+       });
+        Scene scene = new Scene(ballgroup, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setTitle("Bouncing Ball");
         stage.setScene(scene);
+        ballgroup.getChildren().add(title);
         stage.show();
     }
-    /*
-    public boolean borderTouch() {
-        return (this.ballThatBounce.getCenterY() + );
+    public boolean borderTouch(int n) {
+        int i = 0;
+        boolean touch = false;
+        while(i < this.ballThatBounce.size() && !touch) {
+            System.out.println("Numeros "  + i + "et numeros du circle " + n);
+            System.out.println("Border : ");
+            System.out.println( this.ballThatBounce.get(n).iSBorderColideAll(this.WINDOW_WIDTH, this.WINDOW_HEIGHT));
+            System.out.println("Other ball : ");
+            System.out.println(this.ballThatBounce.get(n).Colide(this.ballThatBounce.get(i)));
+            System.out.println("What's suppose to be in touch : ");
+            System.out.println(this.ballThatBounce.get(n).Colide(this.ballThatBounce.get(i)) || this.ballThatBounce.get(n).iSBorderColideAll(this.WINDOW_WIDTH, this.WINDOW_HEIGHT));
+            touch = this.ballThatBounce.get(n).iSBorderColideAll(this.WINDOW_WIDTH, this.WINDOW_HEIGHT);
+            //touch = i != n ? this.ballThatBounce.get(n).Colide(this.ballThatBounce.get(i)) || this.ballThatBounce.get(n).iSBorderColideAll(this.WINDOW_WIDTH, this.WINDOW_HEIGHT) : this.ballThatBounce.get(n).iSBorderColideAll(this.WINDOW_WIDTH, this.WINDOW_HEIGHT);
+            System.out.println("i avant" + i);
+            i++;
+            System.out.println("i après " + i);
+        }
+        System.out.println("touch :");
+        System.out.println(touch);
+        if (touch) System.out.println("Rebond");
+        return touch;
     }
+    public void bouncing(double x, double y) {
+        //this.executor.shutdown();
+        int xSpeed = Math.random() > 0.5 ? 1 : -1;
+        //xSpeed *= Math.random() > 0.5 ? 1 : -1;
+        int ySpeed = Math.random() > 0.5 ? 1 : -1;
+        //ySpeed *= Math.random() > 0.5 ? 1 : -1;
+        this.ballThatBounce.add(new AbleToBounce(x - 12.5, y - 12.5,25.0, xSpeed, ySpeed));
+        int ballNumber = this.ballThatBounce.size() - 1;
+        //GridPane.setConstraints(ballThatBounce.get(ballNumber), (int) ballThatBounce.get(ballNumber).getCenterX(), (int) ballThatBounce.get(ballNumber).getCenterY());
+        this.ballgroup.getChildren().add(ballThatBounce.get(ballNumber));
 
-     */
-    public void bouncing() {
-        int xSpeed = Math.random() > 0.5 ? 1 : 0;
-        xSpeed *= Math.random() > 0.5 ? 1 : -1;
-        int ySpeed = Math.random() > 0.5 ? 1 : 0;
-        ySpeed *= Math.random() > 0.5 ? 1 : -1;
-        if (this.ballThatBounce.getRadius() == 0) this.ballThatBounce.setRadius(25.0);
-        this.ballThatBounce.setCenterX((double) this.WINDOW_HEIGHT / 2);
-        this.ballThatBounce.setCenterY((double) this.WINDOW_WIDTH / 2);
+        /*
+        this.executor.submit(() -> {
+            actualise();
+        });
+         */
+    }
+    public void actualise() {
         while(true) {
-            try {
-                Thread.sleep(1000);
-                this.ballThatBounce.setCenterX(this.ballThatBounce.getCenterX() + xSpeed);
-                this.ballThatBounce.setCenterY(this.ballThatBounce.getCenterY() + ySpeed);
-            } catch (InterruptedException e) {
-                System.out.println("ERRORRR");
+            System.out.println();
+            if (!ballThatBounce.isEmpty()) {
+                for (int i = 0; i < ballThatBounce.size(); i++) {
+                    try {
+                        System.out.println(ballThatBounce.get(i));
+                        Thread.sleep(100);
+                        ballThatBounce.get(i).setCenterX(ballThatBounce.get(i).getCenterX() + ballThatBounce.get(i).getXSpeed());
+                        ballThatBounce.get(i).setCenterY(ballThatBounce.get(i).getCenterY() + ballThatBounce.get(i).getYSpeed());
+                        ballThatBounce.get(i).setTranslateX(ballThatBounce.get(i).getCenterX() + ballThatBounce.get(i).getXSpeed());
+                        ballThatBounce.get(i).setTranslateY(ballThatBounce.get(i).getCenterY() + ballThatBounce.get(i).getYSpeed());
+                        ballThatBounce.get(i).setXSpeed(ballThatBounce.get(i).getXSpeed() * (borderTouch(i) ? -1 : 1));
+                        ballThatBounce.get(i).setYSpeed(ballThatBounce.get(i).getYSpeed() * (borderTouch(i) ? -1 : 1));
+                        System.out.println(borderTouch(i) ? -1 : 1);
+                        //GridPane.setConstraints(ballThatBounce.get(i), (int) ballThatBounce.get(i).getCenterX(), (int) ballThatBounce.get(i).getCenterY());
+                        //System.out.println("yessssssss");
+                    } catch (InterruptedException e) {
+                        System.out.println("ERRORRR");
+                    }
+                }
             }
         }
-    }
+        }
     public static void main(String[] arg) {
         launch(arg);
     }
