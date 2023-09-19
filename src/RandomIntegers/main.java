15ce9fda19042f5a3de1534bfc5e20789942a503
Dylan Quellet <dylan.quellet@efrei.net>
2023-09-19 11:20:48 -0400

    Exo 2 - correction

--- src/RandomIntegers/main.java
+++ src/RandomIntegers/main.java
@@ -9,7 +9,7 @@ public class main {
         SecureRandom randomNumbers = new SecureRandom();
         System.out.println("10 Random numbers:\n");
         int[] randomInt = randomNumbers.ints(10,0,1000).toArray();
-        System.out.printf("Values: %s%n \nOdd: %d%nEven: %d%n \nAverage of all: %.2f%nAverage of odd: %.2f%nAverage of even: %.2f%n",
+        System.out.printf("Values: %s%n \nEven: %d%nOdd: %d%n \nAverage of all: %.2f%nAverage of even: %.2f%nAverage of odd: %.2f%n",
                 IntStream.of(randomInt).mapToObj(String::valueOf).collect(Collectors.joining("   ")),
                 IntStream.of(randomInt).filter(x -> x % 2 == 0).map(x -> 1).sum(),
                 IntStream.of(randomInt).filter(x -> x % 2 == 1).map(x -> 1).sum(),
